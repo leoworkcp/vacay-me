0424bf2651ad3187aee8e9b4e1a23645bedf0e37
leoworkcp <leoworkcp@gmail.com>
2021-02-18 17:49:57 -0500

    phase 1 frontend completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,25 @@
+// frontend/src/App.js
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
+
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    isLoaded && (
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    )
   );
 }
 
