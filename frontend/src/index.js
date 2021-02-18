816617a7d0bead6db85a05b3e08dd0b56808186d
leoworkcp <leoworkcp@gmail.com>
2021-02-18 18:31:44 -0500

    Modal Login Form completed, need to finish some CSS and add a Modal signup form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
+// frontend/src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider as ReduxProvider } from "react-redux";
 import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ModalProvider } from "./context/Modal";
+
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
@@ -18,17 +21,15 @@ if (process.env.NODE_ENV !== "production") {
   window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
-}
-
 function Root() {
   return (
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    <Provider store={store}>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
+    </Provider>
   );
 }
 
